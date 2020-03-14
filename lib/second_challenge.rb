70dada9c9888d6216d34258fc24c01e34e10e9c6
Ty Raino <mstymetris@yahoo.com>
2020-03-14 07:22:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,35 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+    :patriarch => {
 
-  
+    },
+    :matriarch => {
+
+    },
+    :heroine => {
+
+    },
+    :hero_friends => [
+
+    ],
+      },
+    :capulet => {
+      :patriarch => {
+
+      },
+      :matriarch => {
+
+      },
+      :heroine => {
+
+      },
+      :heroine_friends[
+
+      ],
+
+    }
+}
+
+
 end
