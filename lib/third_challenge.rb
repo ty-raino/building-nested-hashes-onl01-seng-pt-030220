c1644f4ddb694b4b23fc656f30f1f0a01368a75e
Ty Raino <mstymetris@yahoo.com>
2020-03-14 07:34:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,28 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
-        :age => "53"
+        name: "Lord Montague"
+        age: "53"
         },
       :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
+        name: "Lady Montague"
+        age: "54"
         },
       :hero => {
-        :name => "Romeo"
-        :age
-=        },
+        name: "Romeo"
+        age: "15"
+        statue: "alive"
+        },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+      name: "Lord Capulet"
+      age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+      name: "Lady Capulet"
+      age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet"
+        age: "15"
+        status: "alive"
         },
       :heroine_friends => []
    }
